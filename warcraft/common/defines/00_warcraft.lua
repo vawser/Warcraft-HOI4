55de8e9be92bb711f75b481bd6d8d1b11f9767aa
Vawser <ben.rouncefield@gmail.com>
2021-01-26 05:02:14 +0000

    Update

--- warcraft/common/defines/00_warcraft.lua
+++ warcraft/common/defines/00_warcraft.lua
@@ -1,64 +1,82 @@
--- Dates
+----------------------------------------------------------
+-- Game
+----------------------------------------------------------
 NDefines.NGame.START_DATE = "592.1.1.1"
 NDefines.NGame.END_DATE = "1000.1.1.1"
-NDefines.NCountry.EVENT_PROCESS_OFFSET = 30
+
+----------------------------------------------------------
+-- Wiki
+----------------------------------------------------------
 NDefines.NWiki.BASE_URL = "https://wow.gamepedia.com/Wowpedia"
 
--- Population
-NDefines.NCountry.POPULATION_YEARLY_GROWTH_BASE = 0.05
+----------------------------------------------------------
+-- Country
+----------------------------------------------------------
+NDefines.NCountry.EVENT_PROCESS_OFFSET = 30
+NDefines.NCountry.POPULATION_YEARLY_GROWTH_BASE = 0.05 -- Increase monthly manpower growth
+NDefines.NCountry.MIN_FOCUSES_FOR_CONTINUOUS = 0 -- Allow continious focuses at any time.
 
+----------------------------------------------------------
 -- Diplomacy
-NDefines.NDiplomacy.IDEOLOGY_JOIN_FACTION_MIN_LEVEL = 2.0
+----------------------------------------------------------
+NDefines.NDiplomacy.IDEOLOGY_JOIN_FACTION_MIN_LEVEL = 2.0 -- Block join faction
 
+----------------------------------------------------------
 -- Buildings
-NDefines.NBuildings.ANTI_AIR_SUPERIORITY_MULT = 2.5
-NDefines.NBuildings.MAX_BUILDING_LEVELS = 10
+----------------------------------------------------------
+NDefines.NBuildings.MAX_BUILDING_LEVELS = 10 -- Reduce max level to 10, a more sensible number.
 
--- Research
-NDefines.NTechnology.BASE_TECH_COST = 15
+----------------------------------------------------------
+-- Technologies
+----------------------------------------------------------
+NDefines.NTechnology.BASE_TECH_COST = 15 -- Reduce base day cost for technologies
 
+----------------------------------------------------------
 -- National Focus
-NDefines.NFocus.FOCUS_POINT_DAYS = 3
-NDefines.NCountry.MIN_FOCUSES_FOR_CONTINUOUS = 0
+----------------------------------------------------------
+NDefines.NFocus.FOCUS_POINT_DAYS = 3 -- Reduce base day cost for focuses
 
+----------------------------------------------------------
 -- Production
-NDefines.NProduction.MIN_POSSIBLE_TRAINING_MANPOWER = 100
+----------------------------------------------------------
+NDefines.NProduction.BASE_FACTORY_SPEED = 1         -- 5
+NDefines.NProduction.BASE_FACTORY_SPEED_MIL = 1     -- 4.5
+NDefines.NProduction.BASE_FACTORY_SPEED_NAV = 1     -- 2.5
 
--- Naval Invasion
-NDefines.NMilitary.AMPHIBIOUS_INVADE_MOVEMENT_COST = 24.0
-NDefines.NNavy.AMPHIBIOUS_LANDING_PENALTY = -0.7        -- amphibious landing penalty
-NDefines.NNavy.AMPHIBIOUS_INVADE_SPEED_BASE = 0.5       -- every hour movement progress on amphibious invasion
-NDefines.NNavy.AMPHIBIOUS_INVADE_MOVEMENT_COST = 24.0   -- total progress cost of movement while amphibious invading
-NDefines.NNavy.AMPHIBIOUS_INVADE_ATTACK_LOW = 0.2 	    -- low and high cap of attack modifier scale. Scale interpolated by invasion progress.
-NDefines.NNavy.AMPHIBIOUS_INVADE_ATTACK_HIGH = 1.0
-NDefines.NNavy.AMPHIBIOUS_INVADE_DEFEND_LOW = 1.5 	    -- low and high cap of defend modifier scale. Scale interpolated by invasion progress.
-NDefines.NNavy.AMPHIBIOUS_INVADE_DEFEND_HIGH = 1.0
-NDefines.NNavy.AMPHIBIOUS_INVADE_LANDING_PENALTY_DECREASE = 3.5 -- scale of bonus that decreases "amphibious penalty" during combat, relative to invading transporter tech.
+NDefines.NProduction.MAX_CIV_FACTORIES_PER_LINE = 100
 
+NDefines.NProduction.MIN_POSSIBLE_TRAINING_MANPOWER = 100 -- Reduce minimum manpower to the new scale
+
+----------------------------------------------------------
+-- Operatives
+----------------------------------------------------------
+NDefines.NOperatives.AGENCY_CREATION_DAYS = 7
+NDefines.NOperatives.AGENCY_UPGRADE_DAYS = 30
+NDefines.NOperatives.AGENCY_CREATION_FACTORIES = 1
+NDefines.NOperatives.AGENCY_UPGRADE_PER_OPERATIVE_SLOT = 1
+NDefines.NOperatives.MAX_OPERATIVE_SLOT_FROM_AGENCY_UPGRADES = 10
+NDefines.NOperatives.OPERATIVE_BASE_INTEL_NETWORK_GAIN = 1.0
+
+----------------------------------------------------------
 -- Military
-NDefines.NMilitary.CORPS_COMMANDER_DIVISIONS_CAP = 35
-NDefines.NMilitary.FIELD_MARSHAL_DIVISIONS_CAP = 35
-NDefines.NMilitary.FIELD_MARSHAL_ARMIES_CAP = 5
-NDefines.NMilitary.MAX_ARMY_EXPERIENCE = 1000
-NDefines.NMilitary.MAX_NAVY_EXPERIENCE = 1000
-NDefines.NMilitary.MAX_AIR_EXPERIENCE = 1000
-NDefines.NMilitary.TRAINING_MAX_LEVEL = 4
-NDefines.NMilitary.DEPLOY_TRAINING_MAX_LEVEL = 2
-NDefines.NMilitary.ARMY_EXP_BASE_LEVEL = 1
-NDefines.NMilitary.UNIT_EXPERIENCE_PER_COMBAT_HOUR = 0.0001
-NDefines.NMilitary.UNIT_EXPERIENCE_PER_TRAINING_DAY = 0.001
+----------------------------------------------------------
+NDefines.NMilitary.CORPS_COMMANDER_DIVISIONS_CAP = 30 -- Increased
+NDefines.NMilitary.FIELD_MARSHAL_DIVISIONS_CAP = 30 -- Increased
+NDefines.NMilitary.MAX_ARMY_EXPERIENCE = 1000 -- Increased
+NDefines.NMilitary.MAX_NAVY_EXPERIENCE = 1000 -- Increased
+NDefines.NMilitary.MAX_AIR_EXPERIENCE = 1000 -- Increased
+NDefines.NMilitary.TRAINING_MAX_LEVEL = 4 -- Increased
+NDefines.NMilitary.DEPLOY_TRAINING_MAX_LEVEL = 2 -- Increased
 NDefines.NMilitary.UNIT_EXP_LEVELS = { 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.75, 0.85, 0.9, 0.95 }
 NDefines.NMilitary.LAND_EQUIPMENT_BASE_COST = 25
-NDefines.NMilitary.LAND_EQUIPMENT_RAMP_COST = 0
+NDefines.NMilitary.LAND_EQUIPMENT_RAMP_COST = 0 -- Removed ramp cost
 NDefines.NMilitary.NAVAL_EQUIPMENT_BASE_COST = 25
-NDefines.NMilitary.NAVAL_EQUIPMENT_RAMP_COST = 0
+NDefines.NMilitary.NAVAL_EQUIPMENT_RAMP_COST = 0 -- Removed ramp cost
 NDefines.NMilitary.AIR_EQUIPMENT_BASE_COST = 25
-NDefines.NMilitary.AIR_EQUIPMENT_RAMP_COST = 0
-NDefines.NMilitary.FIELD_EXPERIENCE_SCALE = 0.08
-NDefines.NMilitary.EXPERIENCE_LOSS_FACTOR = 4.0
+NDefines.NMilitary.AIR_EQUIPMENT_RAMP_COST = 0 -- Removed ramp cost
+NDefines.NMilitary.FIELD_EXPERIENCE_SCALE = 0.08 -- Increased
+NDefines.NMilitary.EXPERIENCE_LOSS_FACTOR = 4.0 -- Increased
 
--- Combat
--- Mostly changed to help AI perform OK
 NDefines.NMilitary.HOURLY_ORG_MOVEMENT_IMPACT = -0.1            -- -0.2
 NDefines.NMilitary.ZERO_ORG_MOVEMENT_MODIFIER = -0.5            -- -0.8
 
@@ -105,19 +123,20 @@ NDefines.NMilitary.PLANNING_MAX = 0.25                          -- 0.3
 NDefines.NMilitary.COMBAT_SUPPLY_LACK_IMPACT = -0.25            -- -0.33
 NDefines.NMilitary.BASE_CAPTURE_EQUIPMENT_RATIO = 0.1           -- 0.0
 
+----------------------------------------------------------
 -- Navy
+----------------------------------------------------------
 NDefines.NNavy.NAVAL_INVASION_PREPARE_HOURS = 24
 
+----------------------------------------------------------
 -- AI
+----------------------------------------------------------
 NDefines.NAI.DIPLOMACY_CREATE_FACTION_FACTOR = 0    -- Limit random faction creation / joining for AI
 NDefines.NAI.DIPLOMACY_STAGE_COUP_COST_FACTOR = 0   -- Limit coup for AI
 NDefines.NAI.DIPLOMACY_ACCEPT_VOLUNTEERS_BASE = 0   -- Limit volunteers for the AI
 
-NDefines.NAI.GENERATE_WARGOAL_THREAT_BASELINE = 0.0
-NDefines.NAI.MIN_ANTAGONIZE_FOR_WARGOAL_JUSTIFICATION = -1000
-NDefines.NAI.WARGOAL_GENERATION_STRENGTH_FACTOR = 2.0
-
-NDefines.NAI.MAX_DISTANCE_NALAV_INVASION = 10000.0
+NDefines.NAI.GENERATE_WARGOAL_THREAT_BASELINE = 0.0 -- Allow AI to start generating wargoals at any time
+NDefines.NAI.MAX_DISTANCE_NALAV_INVASION = 10000.0 -- Increase naval invasion distance for AI due to size of map
 NDefines.NAI.MAX_EXTRA_WARGOAL_GENERATION = 10
 NDefines.NAI.CALL_ALLY_BASE_DESIRE = 100            
 NDefines.NAI.CALL_ALLY_DEMOCRATIC_DESIRE = 0
@@ -225,8 +244,6 @@ NDefines.NAI.DIVISION_DESIGN_WEIGHTS = {
 }
 
 NDefines.NAI.MANPOWER_FREE_USAGE_THRESHOLD = 1
-NDefines.NAI.MANPOWER_RESERVED_THRESHOLD = 0 
-NDefines.NAI.MAX_SUPPLY_DIVISOR = 0.25
 NDefines.NAI.MIN_FIELD_STRENGTH_TO_BUILD_UNITS = 0.01
 NDefines.NAI.MIN_MANPOWER_TO_BUILD_UNITS = 0.01
 NDefines.NAI.DEPLOY_MIN_TRAINING_PEACE_FACTOR = 0.9
@@ -258,7 +275,10 @@ NDefines.NAI.MAX_MICRO_ATTACKS_PER_ORDER = 32
 NDefines.NAI.MIN_UNITS_FACTOR_INVASION_ORDER = 0.1
 NDefines.NAI.MAX_UNITS_FACTOR_INVASION_ORDER = 0.5
 
--- Flag Fix
+----------------------------------------------------------
+-- Graphics
+----------------------------------------------------------
+-- Flag fix
 NDefines.NGraphics.COUNTRY_FLAG_TEX_MAX_SIZE = 2048
 NDefines.NGraphics.COUNTRY_FLAG_SMALL_TEX_MAX_SIZE = 512
 NDefines.NGraphics.COUNTRY_FLAG_STRIPE_TEX_MAX_WIDTH = 10
